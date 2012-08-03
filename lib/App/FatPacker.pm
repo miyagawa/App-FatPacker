2b49e0e2c6a531367e2dff5dad021fd33949b5a2
Karen Etheridge <ether@cpan.org>
2012-08-03 15:09:00 -0700

    prep for release
    ...and switching to dotless versions to make it clear we have a lot of room
    left before we're forced over to 1.0

--- lib/App/FatPacker.pm
+++ lib/App/FatPacker.pm
@@ -13,7 +13,7 @@ use File::Copy qw(copy);
 use File::Path qw(mkpath rmtree);
 use B qw(perlstring);
 
-our $VERSION = '0.009008'; # 0.9.8
+our $VERSION = '0.009009'; # 0.9.9
 
 $VERSION = eval $VERSION;
 
